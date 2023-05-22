4534fb0a45fddfd9076a97d2b2e2767a7e3bb4e1
abukbt13 <abukbt13@gmail.com>
2023-05-22 08:36:02 +0300

    assed min and seconds counter

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var play=document.querySelector('#play')
 var resume=document.querySelector('.resume')
 var pause=document.querySelector('#pause')
 var reset= document.querySelector('#reset')
+var getsecondso1= document.querySelector('#getsecondso1')
+var getseconds2= document.querySelector('#getseconds2')
+var getminutes1= document.querySelector('#getminutes1')
+var getminutes2= document.querySelector('#getminutes2')
+// var getminutes2= document.querySelector('#getminutes2')
 
 var player1=document.querySelector('.player1')
 var player2=document.querySelector('.player2')
@@ -15,62 +20,74 @@ var entertime=document.querySelector('#entertime')
 
 set_time.addEventListener('click',() => {
     show_set_time.style.display='block'
+    // begin_time()
 })
 
 let setmin=0
-let hour=''
-player1time=''
-player2time=''
+let player1time=60
+let player2time=60
+// let getsecondso=''
 
+// let setmin1=''
 
 function begin_time() {
+    let setmino=0
+
 if(min.value==''){
-    setmin=5
-    player1time=setmin
-    player2time=setmin
+    setmin1=5
+    setmin2=5
+    setmino=setmin1
 }
 else {
-    setmin=min.value
-    player1time=setmin
-    player2time=setmin
+    setmin1=min.value
+    setmino=setmin1
+    setmin2=min.value
 }
     show_set_time.style.display='none'
 
-    player1.innerHTML=player2time
-    player2.innerHTML=player1time
+    getminutes1.innerHTML=setmin1
+    getminutes2.innerHTML=setmin2
 }
 let intervals=''
 
 player1.addEventListener('click',() =>{
     clearInterval(intervals)
-    intervals=setInterval(play2time,100)
+    intervals=setInterval(play2timer,100)
 })
 
 player2.addEventListener('click',() =>{
     clearInterval(intervals)
-    intervals=setInterval(play1time,90)
+    intervals=setInterval(play1timer,100)
 })
 
-function play1time() {
-    player1time++;
+function play1timer() {
+    player1time--;
+    if(player1time>9){
+        getsecondso1.innerHTML = player1time
 
-    if (player1time < 10) {
-        player1.innerHTML = '00' + ' : ' + '0' + player1time;
     }
-    if (player1time > 9) {
-        player1.innerHTML = '00' + ' : ' + player1time;
+    if (player1time <9) {
+        getsecondso1.innerHTML = '0'+player1time
     }
-    if (player1time > 59) {
-        hour++
-        player1time = 0;
-        player1.innerHTML = hour + ' : ' + '00';
+    if (player1time <1) {
+        player1time=59
+       setmin1--
+        getsecondso1.innerHTML = player1time
+        getminutes1.innerHTML=setmin1
     }
+
+
 }
 
 
-function play2time(){
-    player2time--
-    player2.innerHTML=player2time + ' : ' +'00'
+function play2timer(){
+    player2time--;
+    if(player2time<9){
+        getseconds2.innerHTML = '0'+player2time
+    }
+    else{
+        getseconds2.innerHTML = player2time
+    }
 
 }
 
